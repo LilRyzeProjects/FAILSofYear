b2e1baa8f6f1b0ec1bfa44ca5772a3715555d4bb
Sergey Bezrodnyi <sergeyandreevich1703@gmail.com>
2018-10-04 15:25:37 +0300

    TestFail_OF_YEAR
    Test

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/page', function() {
+    //echo '<pre>';
+    echo env('APP_ENV');
+    //echo '</pre>';
+
+});
+
+Route::post('/comments', function(){
+    print_r($_POST);
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
